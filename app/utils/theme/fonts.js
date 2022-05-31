6f9d1ee8ae0657e6b8ff3f6e611965316ea6bc7f
VihagaYohan <vihagayohan94@gmail.com>
2022-05-31 23:07:04 +0530

    Added - Font constant file

---
+++ app/utils/theme/fonts.js
@@ -0,0 +1,10 @@
+import {responsiveScreenFontSize,
+responsiveFontSize} from 'react-native-responsive-dimensions'
+
+export default  {
+    extraLarge:responsiveFontSize(2.6),
+    large:responsiveFontSize(2.4),
+    medium:responsiveFontSize(2.3),
+    regular:responsiveFontSize(2),
+    small:responsiveFontSize(1.75)
+}
