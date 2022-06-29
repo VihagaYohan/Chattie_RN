97209f57124fafb64ab4f0ba2eb8c8592e8d6ffc
VihagaYohan <vihagayohan94@gmail.com>
2022-06-29 21:02:08 +0530

    Changed - onPress event in icons

--- app/components/CustomIcons.js
+++ app/components/CustomIcons.js
@@ -1,72 +1,72 @@
 import React, { Component } from 'react'
-import {StyleSheet,View,Text} from 'react-native'
+import { StyleSheet, View, Text } from 'react-native'
 //import {Entypo,FontAwesome} from 'react-native-vector-icons'
 import Entypo from 'react-native-vector-icons/Entypo'
 import FontAwesome from 'react-native-vector-icons/FontAwesome'
 import Ionicons from 'react-native-vector-icons/Ionicons'
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
 
-import {colors,constants} from '../utils'
+import { colors, constants } from '../utils'
 
-const {iconSize} = constants
+const { iconSize } = constants
 
 export const EntypoIcon = ({
     name,
-    color=colors.primaryBlack,
-    size=iconSize,
-    onPress=()=>{return}
-})=>{
-    return(
-        <Entypo 
-        name={name}
-        size={size}
-        color={color}
-        onPress={onPress}/>
+    color = colors.primaryBlack,
+    size = iconSize,
+    ...props
+}) => {
+    return (
+        <Entypo
+            name={name}
+            size={size}
+            color={color}
+            {...props} />
     )
 }
 
 export const FontAwesomeIcon = ({
     name,
-    color=colors.primaryBlack,
-    size=iconSize,
-    onPress=()=>{return}
-})=>{
-    return(
-        <FontAwesome 
-        name={name}
-        size={size}
-        color={color}
-        onPress={onPress}/>
+    color = colors.primaryBlack,
+    size = iconSize,
+    ...props
+}) => {
+    return (
+        <FontAwesome
+            name={name}
+            size={size}
+            color={color}
+            {...props} />
     )
 }
 
 export const IoniconsIcon = ({
     name,
-    color=colors.primaryBlack,
-    size=iconSize,
-    onPress=()=>{return}
-})=>{
-    return(
-        <Ionicons 
-        name={name}
-        size={size}
-        color={color}
-        onPress={onPress}/>
+    color = colors.primaryBlack,
+    size = iconSize,
+    ...props
+}) => {
+    return (
+        <Ionicons
+            name={name}
+            size={size}
+            color={color}
+            {...props} />
     )
 }
 
 export const FontAwesomeIcon5 = ({
     name,
-    color=colors.primaryBlack,
-    size=iconSize,
-    onPress=()=>{return}
-})=>{
-    return(
-        <FontAwesome5 
-        name={name}
-        size={size}
-        color={color}
-        onPress={onPress}/>
+    color = colors.primaryBlack,
+    size = iconSize,
+    ...props
+}) => {
+    return (
+        <FontAwesome5
+            name={name}
+            size={size}
+            color={color}
+            {...props} />
     )
 }
 
