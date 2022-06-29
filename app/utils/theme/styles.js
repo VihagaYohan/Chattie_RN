2eea0103473ee82aeabe1bd252cff202c7447555
VihagaYohan <vihagayohan94@gmail.com>
2022-06-29 21:02:35 +0530

    Added - Shadow style property

--- app/utils/theme/styles.js
+++ app/utils/theme/styles.js
@@ -5,9 +5,20 @@ export const styles = StyleSheet.create({
         backgroundColor: 'blue'
     },
     backgroundColor: 'red',
-    flex_Row:{
-        flexDirection:'row',
-        width:'100%'
+    flex_Row: {
+        flexDirection: 'row',
+        width: '100%'
+    },
+    shadow:
+    {
+        shadowColor: '#7f5df0',
+        shadowOffset: {
+            width: 0,
+            height: 10
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.5,
+        elevation: 5
     }
 })
 
